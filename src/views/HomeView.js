8c1b6353746dd5c78dca6078364f1d59e06840e2
Vadym <vad.evlanov@gmail.com>
2021-01-29 13:20:49 +0200

    raised the menu

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,6 +1,7 @@
 import Hero from '../components/Hero/Hero';
 import About from '../components/About/About';
 import MyService from '../components/MyService/MyService';
+import { Route, Switch } from 'react-router-dom';
 
 export default function HomeView() {
   return (
